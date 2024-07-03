a93f96e9801461e5fe3bd1262336a07b23b1836b
LenonFelipe <lenonfelipe98@gmail.com>
2024-07-03 02:14:43 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+function openMenu(){
+
+    const menuOpen = document.querySelector('#s1');
+    const menuClose = document.querySelector('#s2');
+    const span3 = document.querySelector('#s3');
+
+    span1.style.top = '10px';
+    span1.style.transform = 'rotate(45deg)';
+
+    span2.style.opacity = '0%';
+    
+    span3.style.top = '-10px';
+    span3.style.transform = 'rotate(130deg)';
+}
